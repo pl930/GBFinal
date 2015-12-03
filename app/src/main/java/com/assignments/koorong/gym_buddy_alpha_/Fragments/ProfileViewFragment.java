c2ee6930a3c58a9e8df4c02ca96f4602629f3291
BrandonSaldanha <bbbsaldanha@gmail.com>
2015-12-02 19:39:46 -0500

    got rid of superman glitch

--- app/src/main/java/com/assignments/koorong/gym_buddy_alpha_/Fragments/ProfileViewFragment.java
+++ app/src/main/java/com/assignments/koorong/gym_buddy_alpha_/Fragments/ProfileViewFragment.java
@@ -1,14 +1,18 @@
 package com.assignments.koorong.gym_buddy_alpha_.Fragments;
 
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.FragmentManager;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.provider.Telephony;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -62,11 +66,29 @@ public class ProfileViewFragment extends Fragment {
         setup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent smsIntent = new Intent(Intent.ACTION_VIEW);
-                smsIntent.setType("vnd.android-dir/mms-sms");
-                smsIntent.putExtra("address", "9056916760");
-                smsIntent.putExtra("sms_body","Hey I matched with you on gymbuddy");
-                startActivity(smsIntent);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) //At least KitKat
+                {
+                    Activity activity = getActivity();
+                    String defaultSmsPackageName = Telephony.Sms.getDefaultSmsPackage(activity); //Need to change the build to API 19
+
+                    Intent sendIntent = new Intent(Intent.ACTION_SENDTO, Uri.parse("smsto:" + Uri.encode("9056916760")));
+                    sendIntent.setType("text/plain");
+                    sendIntent.putExtra(Intent.EXTRA_TEXT, "Hey I matched with you on gymbuddy");
+
+                    if (defaultSmsPackageName != null)//Can be null in case that there is no default, then the user would be able to choose any app that support this intent.
+                    {
+                        sendIntent.setPackage(defaultSmsPackageName);
+                    }
+                    activity.startActivity(sendIntent);
+
+                }else {
+
+                    Intent smsIntent = new Intent(Intent.ACTION_VIEW);
+                    smsIntent.setType("vnd.android-dir/mms-sms");
+                    smsIntent.putExtra("address", "9056916760");
+                    smsIntent.putExtra("sms_body", "Hey I matched with you on gymbuddy");
+                    startActivity(smsIntent);
+                }
 
             }
         });
@@ -122,7 +144,16 @@ public class ProfileViewFragment extends Fragment {
                   txtSummary.setText("This user has not entered a summary yet");
                   ImageView imgView = (ImageView)view.findViewById(R.id.profilePic);
                   imgView.setImageResource(R.drawable.face);
+              }else{
+                  TextView txtName = (TextView)view.findViewById(R.id.profileName);
+                  txtName.setText("Superman");
+                  TextView txtSummary = (TextView)view.findViewById(R.id.matchSummary2);
+                  txtSummary.setText("I am the last son of the dead planet Krypton. I am looking for a gym buddy to help me train to fight Doomsday");
+                  ImageView imgView = (ImageView)view.findViewById(R.id.profilePic);
+                  imgView.setImageResource(R.drawable.niccage);
               }
+
+
 //            TextView txtName = (TextView)view.findViewById(R.id.matchName2);
 //            txtName.setText(selectedUser.getName());
 //            TextView txtSummary = (TextView)view.findViewById(R.id.matchSummary2);
